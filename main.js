b089f4771ad8de3053e45c51a11ba3d8bd8aafe9
Your Name <you@example.com>
2022-10-11 18:35:49 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const lib = require("./lib");
+
+if(process.argv.length <= 3) {
+    console.log("IP");
+    process.exit(1);
+}
+
+let command = process.argv[2];
+let numbers = process.argv
+    .slice(3, process.argv.length)
+    .map((n) => parseFloat(n));
+
+if (numbers.some(isNaN)) {
+    console.log("SN");
+    process.exit(1);
+}
+
+let result;
+switch (command) {
+    case "sum": 
+        result = lib.sum(numbers);
+        break;
+    case "avg":
+        result = lib.avg(numbers);
+        break;
+    case "max":
+        result = lib.max(numbers);
+        break;
+    default:        
+}
+
+console.log(result);
